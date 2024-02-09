62c2b63db2fab43d77edf3d033be5e737fa85893
Ankita-Pandey <pankitap.99@gmail.com>
2024-02-09 23:42:32 +0530

    Update resume.pdf

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -192,9 +192,7 @@
 							always aiming to turn data into actionable insights. My approach combines technical
 							skills with a keen understanding of business contexts, ensuring that my contributions
 							are not just technically sound, but also strategically aligned with organizational
-							goals. I am committed to staying at the forefront of technological advancements in
-							data science, continually expanding my skills and knowledge to remain a valuable asset
-							in this rapidly evolving field.
+							goals.
 						</p>
 					</div>
 				</section>
@@ -212,6 +210,154 @@
 					</div>
 					<div>
 						<ol class="group/list">
+							<li class="mb-12">
+								<div
+									class="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50"
+								>
+									<div
+										class="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"
+									/>
+									<header
+										class="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2"
+										aria-label="August 2021 to July 2023"
+									>
+										Oct 2023 — Present
+									</header>
+									<div class="z-10 sm:col-span-6">
+										<h3 class="font-medium leading-snug text-slate-200">
+											<div>
+												<a
+													class="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 group/link text-base"
+													href="/"
+													target="_blank"
+													rel="noreferrer noopener"
+													aria-label="ML Intern at Virginia Tech, Sanghani Center for AI and Data Analytics(opens in a new tab)"
+													><span
+														class="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"
+													/><span
+														>ML Intern ·<!-- -->
+														<span class="inline-block"
+															>Virginia Tech, Sanghani Center for AI and Data Analytics
+															<!-- <svg
+																xmlns="http://www.w3.org/2000/svg"
+																viewBox="0 0 20 20"
+																fill="currentColor"
+																class="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
+																aria-hidden="true"
+																><path
+																	fill-rule="evenodd"
+																	d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+																	clip-rule="evenodd"
+																/></svg> -->
+														</span></span
+													></a
+												>
+											</div>
+										</h3>
+										<p class="mt-2 text-sm leading-normal text-justify">
+											• Optimized data retrieval using NLP techniques and the RAG model,
+											collaborating with company professionals for a 25% efficiency boost. <br />•
+											Spearheaded the development of a project website for NLP-based RAG model,
+											elevating project visibility by 30%
+										</p>
+										<!-- <ul class="mt-2 flex flex-wrap" aria-label="Related links">
+											<li class="mr-4">
+												<a
+													class="relative mt-2 inline-flex items-center text-sm font-medium text-slate-300 hover:text-teal-300 focus-visible:text-teal-300"
+													href="https://developer.apple.com/documentation/musickitjs"
+													target="_blank"
+													rel="noreferrer noopener"
+													aria-label="MusicKit.js (opens in a new tab)"
+													><svg
+														xmlns="http://www.w3.org/2000/svg"
+														viewBox="0 0 20 20"
+														fill="currentColor"
+														class="mr-1 h-3 w-3"
+														aria-hidden="true"
+														><path
+															d="M12.232 4.232a2.5 2.5 0 013.536 3.536l-1.225 1.224a.75.75 0 001.061 1.06l1.224-1.224a4 4 0 00-5.656-5.656l-3 3a4 4 0 00.225 5.865.75.75 0 00.977-1.138 2.5 2.5 0 01-.142-3.667l3-3z"
+														/><path
+															d="M11.603 7.963a.75.75 0 00-.977 1.138 2.5 2.5 0 01.142 3.667l-3 3a2.5 2.5 0 01-3.536-3.536l1.225-1.224a.75.75 0 00-1.061-1.06l-1.224 1.224a4 4 0 105.656 5.656l3-3a4 4 0 00-.225-5.865z"
+														/></svg
+													><span>MusicKit.js</span></a
+												>
+											</li>
+											<li class="mr-4">
+												<a
+													class="relative mt-2 inline-flex items-center text-sm font-medium text-slate-300 hover:text-teal-300 focus-visible:text-teal-300"
+													href="https://9to5mac.com/2018/06/03/apple-music-embeddable-web-player-listen-browser/"
+													target="_blank"
+													rel="noreferrer noopener"
+													aria-label="9to5Mac (opens in a new tab)"
+													><svg
+														xmlns="http://www.w3.org/2000/svg"
+														viewBox="0 0 20 20"
+														fill="currentColor"
+														class="mr-1 h-3 w-3"
+														aria-hidden="true"
+														><path
+															d="M12.232 4.232a2.5 2.5 0 013.536 3.536l-1.225 1.224a.75.75 0 001.061 1.06l1.224-1.224a4 4 0 00-5.656-5.656l-3 3a4 4 0 00.225 5.865.75.75 0 00.977-1.138 2.5 2.5 0 01-.142-3.667l3-3z"
+														/><path
+															d="M11.603 7.963a.75.75 0 00-.977 1.138 2.5 2.5 0 01.142 3.667l-3 3a2.5 2.5 0 01-3.536-3.536l1.225-1.224a.75.75 0 00-1.061-1.06l-1.224 1.224a4 4 0 105.656 5.656l3-3a4 4 0 00-.225-5.865z"
+														/></svg
+													><span>9to5Mac</span></a
+												>
+											</li>
+											<li class="mr-4">
+												<a
+													class="relative mt-2 inline-flex items-center text-sm font-medium text-slate-300 hover:text-teal-300 focus-visible:text-teal-300"
+													href="https://www.theverge.com/2017/10/5/16433770/facebook-messenger-apple-music-bot-song-streaming"
+													target="_blank"
+													rel="noreferrer noopener"
+													aria-label="The Verge (opens in a new tab)"
+													><svg
+														xmlns="http://www.w3.org/2000/svg"
+														viewBox="0 0 20 20"
+														fill="currentColor"
+														class="mr-1 h-3 w-3"
+														aria-hidden="true"
+														><path
+															d="M12.232 4.232a2.5 2.5 0 013.536 3.536l-1.225 1.224a.75.75 0 001.061 1.06l1.224-1.224a4 4 0 00-5.656-5.656l-3 3a4 4 0 00.225 5.865.75.75 0 00.977-1.138 2.5 2.5 0 01-.142-3.667l3-3z"
+														/><path
+															d="M11.603 7.963a.75.75 0 00-.977 1.138 2.5 2.5 0 01.142 3.667l-3 3a2.5 2.5 0 01-3.536-3.536l1.225-1.224a.75.75 0 00-1.061-1.06l-1.224 1.224a4 4 0 105.656 5.656l3-3a4 4 0 00-.225-5.865z"
+														/></svg
+													><span>The Verge</span></a
+												>
+											</li>
+										</ul>
+										<ul class="mt-2 flex flex-wrap" aria-label="Technologies used">
+											<li class="mr-1.5 mt-2">
+												<div
+													class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300"
+												>
+													Ember
+												</div>
+											</li>
+											<li class="mr-1.5 mt-2">
+												<div
+													class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300"
+												>
+													SCSS
+												</div>
+											</li>
+											<li class="mr-1.5 mt-2">
+												<div
+													class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300"
+												>
+													JavaScript
+												</div>
+											</li>
+											<li class="mr-1.5 mt-2">
+												<div
+													class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300"
+												>
+													MusicKit.js
+												</div>
+											</li>
+										</ul> -->
+									</div>
+								</div>
+							</li>
 							<li class="mb-12">
 								<div
 									class="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50"
@@ -259,7 +405,7 @@
 										<p class="mt-2 text-sm leading-normal text-justify">
 											• Innovatively developed an analytics tool using Tableau to validate the
 											authenticity of internal business data against the source data, providing
-											actionable insights to business stakeholders. <br />• Spearheaded the
+											actionable insights to business stakeholders <br />• Spearheaded the
 											development of a data pipeline for the NGCB project, significantly reducing
 											the customer records comparison processing time by 85%, from 120s to 18s.<br
 											/>• Demonstrated leadership by supervising two developers, creating a
@@ -902,7 +1048,7 @@
 						</div> -->
 					</div>
 				</section>
-		
+
 				<footer class="max-w-md pb-16 text-sm text-slate-500 sm:pb-0">
 					<p>
 						Loosely designed in<!-- -->
@@ -962,7 +1108,6 @@
 					</p>
 				</footer>
 			</main>
-		
 		</div>
 	</div>
 </div>
